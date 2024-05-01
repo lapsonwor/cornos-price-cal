71b479be573be54e381ae95e17620f23b1bc5572
Lapson Wong <lapsonwor@gmail.com>
2024-05-01 21:25:00 +0800

    completed the price calculation part

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,117 @@ import Switch from "@mui/material/Switch";
 import DoneIcon from '@mui/icons-material/Done';
 import { border, borderRadius, padding } from "@mui/system";
 import { styled } from '@mui/material/styles';
+import { useState, useEffect } from "react";
+
+export default function Home() {
+  const [pricePerMonth, setPricePerMonth] = useState(8);
+  const [pageViews, setPageViews] = useState('10K');
+  const [pageViewCat, setPageViewCat] = useState(1);
+  const [billType, setBillType] = useState(1);
+
+  useEffect(() => {
+    switch (pageViewCat) {
+      case 1:
+        if (billType === 1) {
+          setPricePerMonth(8);
+        } else {
+          let price = 8 * 0.75;
+          setPricePerMonth(price);
+        }
+        setPageViews('10K');
+        break;
+      case 2:
+        if (billType === 1) {
+          setPricePerMonth(12);
+        } else {
+          let price = 12 * 0.75;
+          setPricePerMonth(price);
+        }
+        setPageViews('50K');
+        break;
+      case 3:
+        if (billType === 1) {
+          setPricePerMonth(16);
+        } else {
+          let price = 16 * 0.75;
+          setPricePerMonth(price);
+        }
+        setPageViews('100K');
+        break;
+      case 4:
+        if (billType === 1) {
+          setPricePerMonth(24);
+        } else {
+          let price = 24 * 0.75;
+          setPricePerMonth(price);
+        }
+        setPageViews('500K');
+        break;
+      case 5:
+        if (billType === 1) {
+          setPricePerMonth(36);
+        } else {
+          let price = 36 * 0.75;
+          setPricePerMonth(price);
+        }
+        setPageViews('1M');
+        break;
+      default:
+        setPricePerMonth(8);
+        setPageViews('10K');
+    }
+  }, [pageViewCat, billType]);
+
+  return (
+    <main className="flex min-h-screen flex-col items-center text-[#8b909e] font-bold text-[15px] sm:px-6 sm:py-16 lg:px-24 lg:py-24" style={mainBg}>
+      <div style={headerBg} className="py-10">
+        <h1 className="text-[#29304d] text-center sm:text-[20px] lg:text-[36px]">Simple, traffic-based pricing</h1>
+        <div className="mt-2 text-center">Sign-up for our 30-day trial.<br className="md:hidden" /> No credit card required.</div>
+      </div>
+      <div className="bg-white z-10 sm:mt-8 md:mt-16 pt-6 pb-5 flex flex-col rounded-xl shadow-md min-w-[55vw]">
+        <div className="sm:mx-6 md:mx-10 flex justify-between items-center sm:flex-col md:flex-row">
+          <div>{pageViews} PAGEVIEWS</div>
+          <div className="sm:hidden md:block">
+            <div className="flex items-center">
+              <div className="text-[#29304d] text-[45px] mr-2">${pricePerMonth}.00</div>
+              <div> / month</div>
+            </div>
+            <div className="text-right">or ~ 32 CRO / month</div>
+          </div>
+        </div>
+        <div className="sm:mx-6 md:mx-10">
+          <Slider sx={sliderColr} defaultValue={1} aria-labelledby="discrete-slider" valueLabelDisplay="auto" step={1} min={1} max={4} value={pageViewCat} onChange={(e, v) => setPageViewCat(v)} valueLabelFormat={value => `${pageViews} PAGEVIEWS`} />
+        </div>
+        <div className="sm:block md:hidden mx-[68px]">
+          <div className="flex items-center justify-center">
+            <div className="text-[#29304d] text-[38px] mr-2">$16.00</div>
+            <div> / month</div>
+          </div>
+          <div className="text-right">or ~ 32 CRO / month</div>
+        </div>
+        <div className="my-10 justify-end mr-10 flex items-center sm:mx-6 md:mx-10 sm:text-[11px] md:text-[15px]">
+          <div>Monthly Biling</div>
+          <Switch sx={switchStyle} checked={billType === 2}
+            onChange={() => setBillType(billType === 1 ? 2 : 1)} />
+          <div>Yearly Biling</div>
+          <div className="text-[#eaab97] px-2 py-1 bg-[#faeee9] rounded-full ml-2 md:hidden">-25%</div>
+          <div className="text-[#eaab97] px-2 py-1 bg-[#faeee9] rounded-full ml-2 sm:hidden md:block">25% discount</div>
+        </div>
+        <hr />
+        <div className="sm:my-4 md:my-8 sm:mx-6 md:mx-10 flex items-center justify-between sm:flex-col md:flex-row">
+          <div className="">
+            <div className="flex mb-2 sm:justify-center md:justify-start"><DoneIcon sx={{ color: "#63d5c5" }} /><div className="ml-4">Unlimited websites</div></div>
+            <div className="flex mb-2 sm:justify-center md:justify-start"><DoneIcon sx={{ color: "#63d5c5" }} /><div className="ml-4">100% data ownership</div></div>
+            <div className="flex sm:justify-center md:justify-start"><DoneIcon sx={{ color: "#63d5c5" }} /><div className="ml-4">Email reports</div></div>
+          </div>
+          <div role="button" className="bg-[#2a3352] text-white rounded-full px-12 py-4 sm:mt-8">
+            Start my trial
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
 
 const sliderColr = {
   color: '#b3f0e9',
@@ -71,56 +182,4 @@ const switchStyle = {
     height: '20px',
     transform: 'translateY(1px) translateX(5px)',
   },
-}
-
-
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center text-[#8b909e] font-bold text-[15px] sm:px-6 sm:py-16 lg:px-24 lg:py-24" style={mainBg}>
-      <div style={headerBg} className="py-10">
-        <h1 className="text-[#29304d] text-center sm:text-[20px] lg:text-[36px]">Simple, traffic-based pricing</h1>
-        <div className="mt-2 text-center">Sign-up for our 30-day trial.<br className="md:hidden" /> No credit card required.</div>
-      </div>
-      <div className="bg-white z-10 sm:mt-8 md:mt-16 pt-6 pb-5 flex flex-col rounded-xl shadow-md min-w-[55vw]">
-        <div className="sm:mx-6 md:mx-10 flex justify-between items-center sm:flex-col md:flex-row">
-          <div>1OOK PAGEVIEWS</div>
-          <div className="sm:hidden md:block">
-            <div className="flex items-center">
-              <div className="text-[#29304d] text-[45px] mr-2">$16.00</div>
-              <div> / month</div>
-            </div>
-            <div className="text-right">or ~ 32 CRO / month</div>
-          </div>
-        </div>
-        <div className="sm:mx-6 md:mx-10">
-          <Slider sx={sliderColr} defaultValue={30} aria-labelledby="discrete-slider" valueLabelDisplay="auto" step={10} min={10} max={110} />
-        </div>
-        <div className="sm:block md:hidden mx-[68px]">
-          <div className="flex items-center justify-center">
-            <div className="text-[#29304d] text-[38px] mr-2">$16.00</div>
-            <div> / month</div>
-          </div>
-          <div className="text-right">or ~ 32 CRO / month</div>
-        </div>
-        <div className="my-10 justify-end mr-10 flex items-center sm:mx-6 md:mx-10 sm:text-[11px] md:text-[15px]">
-          <div>Monthly Biling</div>
-          <Switch sx={switchStyle} />
-          <div>Yearly Biling</div>
-          <div className="text-[#eaab97] px-2 py-1 bg-[#faeee9] rounded-full ml-2 md:hidden">-25%</div>
-          <div className="text-[#eaab97] px-2 py-1 bg-[#faeee9] rounded-full ml-2 sm:hidden md:block">25% discount</div>
-        </div>
-        <hr />
-        <div className="sm:my-4 md:my-8 sm:mx-6 md:mx-10 flex items-center justify-between sm:flex-col md:flex-row">
-          <div className="">
-            <div className="flex mb-2 sm:justify-center md:justify-start"><DoneIcon sx={{ color: "#63d5c5" }} /><div className="ml-4">Unlimited websites</div></div>
-            <div className="flex mb-2 sm:justify-center md:justify-start"><DoneIcon sx={{ color: "#63d5c5" }} /><div className="ml-4">100% data ownership</div></div>
-            <div className="flex sm:justify-center md:justify-start"><DoneIcon sx={{ color: "#63d5c5" }} /><div className="ml-4">Email reports</div></div>
-          </div>
-          <div role="button" className="bg-[#2a3352] text-white rounded-full px-12 py-4 sm:mt-8">
-            Start my trial
-          </div>
-        </div>
-      </div>
-    </main>
-  );
 }
